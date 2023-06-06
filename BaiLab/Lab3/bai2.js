0944ec25b0e11c719caf8c55799927d913cb0ad0
ngochai68 <90449421+ngochai68@users.noreply.github.com>
2023-06-07 00:42:31 +0700

    update

---
+++ BaiLab/Lab3/bai2.js
@@ -0,0 +1,18 @@
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliacorrected = dogsJulia.slice();
+  dogsJuliacorrected.splice(0, 1);
+  dogsJuliacorrected.splice(-2);
+
+  const dogs = dogsJuliacorrected.concat(dogsKate);
+  console.log(dogs);
+
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy`);
+    }
+  });
+};
+
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
